27259f6d63abf0809c58c265b9af3fd2efdaa47b
Artemio Cabelin <artemiocabelin@gmail.com>
2017-07-25 13:50:06 -0700

    added cart and order models

--- apps/orders_app/models.py
+++ apps/orders_app/models.py
@@ -3,3 +3,44 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class ShoppingCart(models.Model):
+    total = models.IntegerField()
+    products = models.ManyToManyField(Product, related_name="orders")
+    user = models.ForeignKey(User, related_name="orders")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Order(models.Model):
+    shoppingCart = models.OneToOneField(ShoppingCart, related_name="order")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ShippingInfo(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255,null=True,blank=True)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zipcode = models.IntegerField(max_length=5)
+    user = models.ForeignKey(User, related_name="shippingInfos")
+    order = models.OneToOneField(Order, related_name="shippingInfo")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class BillingInfo(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    address2 = models.CharField(max_length=255,null=True,blank=True)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zipcode = models.IntegerField(max_length=5)
+    card = models.IntegerField()
+    security = models.IntegerField()
+    expiration = models.DateField()
+    user = models.ForeignKey(User, related_name="billingInfos")
+    order = models.OneToOneField(User, related_name="billingInfo")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- apps/orders_app/views.py
+++ apps/orders_app/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render, redirect, HttpResponse
 # Create your views here.
 def shoppingCart(request):
     return render(request, 'orders_app/shoppingcart.html')
+
