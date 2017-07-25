2bd81ed7d44e3f2ab3234c43c6d9399a1fba7eba
Rommel Castro <Rommel@Rommels-MacBook-Pro-2.local>
2017-07-25 16:30:32 -0700

    changed pagination implementation

--- apps/products_app/urls.py
+++ apps/products_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.home, name='home'),
-    url(r'^addtocart$', views.home, name='home'),
+    url(r'^addtocart$', views.addtocart, name='addtocart'),
     url(r'^(?P<id>[0-9]+)/(?P<page>[0-9]+)$', views.category, name='category'),
     url(r'^product/(?P<id>[0-9]+)$', views.product, name='product'),
     url(r'^search/(?P<searchname>[a-zA-Z]+)/(?P<page>[0-9]+)$', views.search, name='search'),
--- apps/products_app/views.py
+++ apps/products_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from models import *
 # Create your views here.
 def home(request):
@@ -9,37 +9,40 @@ def home(request):
 
 def category(request, id, page):
     allProducts=[]
-    products=Category.objects.get(id=id).products
-    for i in range(10*(page-1), 10*(page-1) + 11): #product in category.products:
-        if i < len(products):
-            allProducts.append([products[i].name, products[i].price, products[i].image])
+    productCount=Category.objects.get(id=id).products.count()
+    allProducts=Category.objects.get(id=id).products[(page-1)*10:((page-1)*10)+10]
+    # for i in range(10*(page-1), 10*(page-1) + 11): #product in category.products:
+    #     if i < len(products):
+    #         allProducts.append([products[i].name, products[i].price, products[i].image])
 
     context={
-        'products':allProducts, 'numPages':len(products)/10, 'category': Category.objects.get(id=id)
+        'products':allProducts, 'numPages':productCount/10, 'category': Category.objects.get(id=id)
     }
     return render(request, 'products_app/listproducts.html', context)
 
 def allprod(request, page):
-    allProducts=[]
-    products=Product.objects.all()
-    for i in range(10*(page-1), 10*(page-1) + 11): #product in category.products:
-        if i < len(products):
-            allProducts.append([products[i].name, products[i].price, products[i].image])
+    # allProducts=[]
+    productCount=Product.objects.all().count()
+    allProducts=Product.objects.all()[(page-1)*10:((page-1)*10)+10]
+    # for i in range(10*(page-1), 10*(page-1) + 11): #product in category.products:
+    #     if i < len(products):
+    #         allProducts.append([products[i].name, products[i].price, products[i].image])
 
     context={
-        'products':allProducts, 'numPages':len(products)/10, 'category': Category.objects.get(id=id)
+        'products':allProducts, 'numPages':productCount/10, 'category': Category.objects.get(id=id)
     }
     return render(request, 'products_app/listproducts.html', context)
 
 def search(request, searchname, page):
-    allProducts=[]
-    products=Product.objects.filter(name__startswith=searchname)
-    for i in range(10*(page-1), 10*(page-1) + 11): #product in category.products:
-        if i < len(products):
-            allProducts.append([products[i].name, products[i].price, products[i].image])
+    # allProducts=[]
+    productCount=Product.objects.filter(name__startswith=searchname).count()
+    allProducts=Product.objects.filter(name__startswith=searchname)[(page-1)*10:((page-1)*10)+10]
+    # for i in range(10*(page-1), 10*(page-1) + 11): #product in category.products:
+    #     if i < len(products):
+    #         allProducts.append([products[i].name, products[i].price, products[i].image])
 
     context={
-        'products':allProducts, 'numPages':len(products)/10, 'searchitem': searchname, 
+        'products':allProducts, 'numPages':productCount/10, 'searchitem':searchname, 
     }
     return render(request, 'products_app/listproducts.html', context)
 
@@ -48,3 +51,10 @@ def product(request, id):
         'product':Product.objects.get(id=id)
     }
     return render(request, 'products_app/productpage.html', context)
+
+def addtocart(request):
+    prod = Product.objects.get(id=id)
+    prod.quantity=request.POST['quantity']
+    prod.save()
+    User.objects.get(id=request.session['user_id']).shoppingCart.products.add(prod)
+    return redirect('prodcuts/all/1')
