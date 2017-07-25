8ac180208fa0295bb8b102784192f3d2975bed41
Rommel Castro <Rommel@Rommels-MacBook-Pro-2.local>
2017-07-25 14:27:56 -0700

    changed models fields

--- apps/products_app/models.py
+++ apps/products_app/models.py
@@ -3,18 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
 class Product(models.Model):
     name=models.CharField(max_length=255)
     description=models.TextField()
-    price=models.DecimalField(decimal_places=2)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
     quantity=models.IntegerField()
-    image=models.CharField()
+    image=models.CharField(max_length=255)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
     category=models.ForeignKey(Category, related_name='products')
 
-class Category(models.Model):
-    name=models.CharField(max_length=255)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
--- apps/products_app/urls.py
+++ apps/products_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns=[
     url(r'^$', views.home, name='home'),
     url(r'^(?P<id>[0-9]+)/(?P<page>[0-9]+)$', views.category, name='category'),
     url(r'^products/(?P<id>[0-9]+)$', views.product, name='product'),
-    url(r'^search/(?P<page>[0-9]+)$', views.search, name='search'),
+    url(r'^search/(?P<searchname>[a-zA-Z]+)/(?P<page>[0-9]+)$', views.search, name='search'),
 ]
 
