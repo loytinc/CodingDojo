25867c1d0f530db018c7124ef21297cc5d0beec9
Rommel Castro <Rommel@Rommels-MacBook-Pro-2.local>
2017-07-27 10:25:36 -0700

    not much

--- apps/products_app/views.py
+++ apps/products_app/views.py
@@ -20,7 +20,7 @@ def category(request, id, page):
     for i in range(1,int(math.ceil((productCount/12.0)) + 1)):
         newArr.append(i)
     context={
-        'products':allProducts, 'numPages':newArr, 'category': Category.objects.get(id=id)
+        'products':allProducts, 'numPages':newArr, 'category': Category.objects.get(id=id), 'currPage':page
     }
     return render(request, 'products_app/listproducts.html', context)
 
@@ -33,7 +33,7 @@ def allprod(request, page):
         newArr.append(i)
 
     context = {
-        'products':allProducts, 'numPages':newArr
+        'products':allProducts, 'numPages':newArr, 'currPage':page
     }
     return render(request, 'products_app/listproducts.html', context)
 
